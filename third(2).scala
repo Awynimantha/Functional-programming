da9b53040730210a088f612df4c3934620fdbbf7
awynimantha <awynimantha@gmail.com>
2022-07-09 16:38:16 +0530

    first

---
+++ third(2).scala
@@ -0,0 +1,19 @@
+
+
+object q3 extends App{
+    var a:Int=2
+     var b:Int=3
+     var c:Int=4
+     var d:Int=5
+     var k:Float=43.0f
+     var g:Float=4.0f
+     println((b-1) * a + c *(d-1))
+     println(a+1)
+     println (-2*(g-k)+c)
+     c=c+1
+     println (c);
+     c=(1+c)*(a+1)
+     println (c)
+}
+
+   
